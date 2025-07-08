fbc931594499b55b8086af3cb8e38640a7f14944
googlehater <xet.1089@gmail.com>
2025-07-08 14:03:09 +0300

    order_models

--- models/order.py
+++ models/order.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, ForeignKey, Text, Integer, Boolean
+from sqlalchemy import String, ForeignKey, Text, Integer, Boolean, Numeric
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import TYPE_CHECKING
 
@@ -6,13 +6,25 @@ from .base import Base
 
 if TYPE_CHECKING:
     from .object import Object
+    from .user import User
 
 
 class Order(Base):
 
     __tablename__ = "orders"
 
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     object_id: Mapped[int] = mapped_column(ForeignKey('objects.id', ondelet="CASCADE"))
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id', ondelet="CASCADE"))
+    system_type: Mapped[str] = mapped_column(String(70))  # enum
+    order_status: Mapped[str] = mapped_column(String(50))  # enum
+    total_price: Mapped[float] = mapped_column(Numeric(10, 2))
+    agreed: Mapped[str] = mapped_column(String(50))
+    priority: Mapped[str] = mapped_column(String(50))  # enum
+    description: Mapped[str] = mapped_column(Text)
+    comment: Mapped[str] = mapped_column(Text)
+    decline_reason: Mapped[str] = mapped_column(String(255))
 
     # связи
     object: Mapped[list["Object"]] = relationship(back_populates="orders")
+    user: Mapped[list["User"]] = relationship(back_populates="orders")
