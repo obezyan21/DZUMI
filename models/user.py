cddb8ac15213c84df271913ad76f6fd3078bd774
googlehater <xet.1089@gmail.com>
2025-07-08 14:47:44 +0300

    user

---
+++ models/user.py
@@ -0,0 +1,30 @@
+from sqlalchemy import String, ForeignKey, Text, Integer, Boolean, Numeric
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING
+
+from .base import Base
+
+if TYPE_CHECKING:
+    from .role import Role
+
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    last_name: Mapped[str] = mapped_column(String(100))
+    first_name: Mapped[str] = mapped_column(String(100))
+    middle_name: Mapped[str] = mapped_column(String(100))
+    email: Mapped[str] = mapped_column(String(255))
+    phone_number: Mapped[str] = mapped_column(String(20))
+    role_id: Mapped[str] = mapped_column(ForeignKey("roles.id", ondelete="CASCADE"))
+    is_active: Mapped[bool] = mapped_column(Boolean)
+
+    # связи
+    role: Mapped[list["Role"]] = relationship(back_populates="users")
+
+
+    
+    
+    
