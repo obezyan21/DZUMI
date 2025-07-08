d2c94c9e40a33cb7b0d10be884b7745812a67dfe
tima010905@gmail.com <tima010905@gmail.com>
2025-07-08 13:11:33 +0300

    "init + BaseDAO"

---
+++ dao/__init__.py
@@ -0,0 +1,23 @@
+from dao.base_dao import BaseDAO
+from dao.documents_dao import DocumentsDao
+from dao.documet_attributes_dao import DocumetAttributesDao
+from dao.items_dao import IitemsDao
+from dao.objects_dao import ObjectsDao
+from dao.order_items_dao import OrderItemsDao
+from dao.orders_dao import OrdersDao
+from dao.roles_dao import RolesDao
+from dao.trusted_suppliers_dao import TrustedSuppliersDao
+from dao.users_dao import UsersDao
+
+__all__ = [
+    'BaseDao',
+    'DocumentsDao',
+    'DocumetAttributesDao',
+    'IitemsDao',
+    'ObjectsDao',
+    'OrderItemsDao',
+    'OrdersDao',
+    'RolesDao',
+    'TrustedSuppliersDao',
+    'UsersDao',
+]
