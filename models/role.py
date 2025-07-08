d1377ab3b6b22da6745c506006c85b3ec3dbfaa7
googlehater <xet.1089@gmail.com>
2025-07-08 14:49:31 +0300

    role_model

---
+++ models/role.py
@@ -0,0 +1,20 @@
+from sqlalchemy import String, ForeignKey, Text, Integer, Boolean, Numeric
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING
+
+from .base import Base
+
+if TYPE_CHECKING:
+    from .role import Role
+
+
+class Role(Base):
+
+    __tablename__ = "roles"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    role_name: Mapped[str] = mapped_column(String(100))
+    description: Mapped[str] = mapped_column(Text)
+
+    # Связь 
+    users: Mapped[list["User"]] = relationship(back_populates="role")
