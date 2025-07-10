63c11637087645f4b9f0267472e73fce6b4f1eb4
tima010905@gmail.com <tima010905@gmail.com>
2025-07-10 12:32:24 +0300

    "get_by_priority func + start patterns"

--- dao/orders_dao.py
+++ dao/orders_dao.py
@@ -6,3 +6,18 @@ from .base_dao import BaseDAO
 class Order(BaseDAO):
     def __init__(self, session):
         super().__init__(session, Order)
+
+
+    def get_by_priority(self, priority):
+        return self.session.query(Order).filter(Order.priority == priority).all() # self.session.query(Order)
+                                                                                  # self.session - это сессия SQLAlchemy, подключение к БД
+                                                                                  # .query(Order) - создаёт новый запрос (Query object) для модели Order
+                                                                                  # .filter(Order.priority == priority)
+                                                                                  # Order.priority - ссылается на столбец priority в таблице orders
+                                                                                  # == priority - сравнение с переданным значением
+                                                                                  # .all() Выполняет собранный запрос в БД, Возвращает все строки результата как список объектов Order
+                                                                                  # Если нет результатов - вернёт пустой список
+                                                                                  # Это типо базовая конструкция стоит на заметку взять
+
+    def get_by_date(self, date):
+        pass
