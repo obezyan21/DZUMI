8f7554291ca9a12a8507cfb056617f15e183b981
googlehater <xet.1089@gmail.com>
2025-07-25 20:56:24 +0300

    gwt_by_id

--- config.py
+++ config.py
@@ -9,7 +9,8 @@ load_dotenv()
 # def main():
 
 string_con = f"{os.getenv('DB_DRIVER')}://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-engine = create_engine(string_con)
+engine = create_engine(string_con,
+                       echo=True)
 session = Session(engine)
 
 
